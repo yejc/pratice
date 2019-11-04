c8469553e5ed4f73bea8c9aa8b277c433c6a66b3
yejc <38127154+yejc@users.noreply.github.com>
2019-11-04 11:25:23 +0800

    Create ConcreteCondition.java

---
+++ src/alibaba/ConcreteCondition.java
@@ -0,0 +1,23 @@
+package alibaba;
+
+import lombok.Data;
+
+import java.io.File;
+import java.util.function.Predicate;
+
+/**
+ * @author yejc001
+ * @date 2019-11-04
+ */
+@Data
+public class ConcreteCondition implements Condition
+{
+
+    private Predicate<File> predicate;
+
+    @Override
+    public boolean judge(File file)
+    {
+        return predicate.test(file);
+    }
+}
