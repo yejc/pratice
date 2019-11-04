b64c6e2c36bd703399fe863fd96379246271e52e
yejc <38127154+yejc@users.noreply.github.com>
2019-11-04 11:25:44 +0800

    Create AndDecorator.java

---
+++ src/alibaba/AndDecorator.java
@@ -0,0 +1,22 @@
+package alibaba;
+
+import lombok.Data;
+
+import java.io.File;
+
+/**
+ * @author yejc001
+ * @date 2019-11-04
+ */
+@Data
+public class AndDecorator implements Condition
+{
+    private Condition condition1;
+    private Condition condition2;
+
+    @Override
+    public boolean judge(File file)
+    {
+        return condition1.judge(file) && condition2.judge(file);
+    }
+}
