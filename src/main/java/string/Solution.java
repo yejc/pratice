cdcf26d83f5f147dbf40e82f684893c82163ecea
yejc <yejc1995@foxmail.com>
2019-11-10 18:39:33 +0800

    feat：字符串练习part2

--- src/main/java/string/Solution.java
+++ src/main/java/string/Solution.java
@@ -69,4 +69,63 @@ public class Solution {
         return indexs.size() == 0 ? -1 : indexs.get(0);
     }
 
+    /**
+     * 有效的字母异位词
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            Integer count = map.get(t.charAt(i));
+            if (count == null || count == 0) {
+                return false;
+            }
+            if (count - 1 == 0) {
+                map.remove(t.charAt(i));
+            } else {
+                map.put(t.charAt(i), count - 1);
+            }
+        }
+        return map.isEmpty();
+    }
+
+    /**
+     * 验证回文字符串
+     * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+     * 说明：本题中，我们将空字符串定义为有效的回文串。
+     * <p>
+     * 示例 1:
+     * 输入: "A man, a plan, a canal: Panama"
+     * 输出: true
+     *
+     * @param s
+     * @return
+     */
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
 }
