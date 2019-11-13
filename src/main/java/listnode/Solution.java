b94e2e792afa95d48a2ff7a09092b3c95d6dc2c4
yejc <yejc1995@foxmail.com>
2019-11-13 21:27:58 +0800

    feat：链表练习part2

--- src/main/java/listnode/Solution.java
+++ src/main/java/listnode/Solution.java
@@ -38,4 +38,82 @@ public class Solution {
         curr.next = null;
         return head;
     }
+
+    /**
+     * 反转链表（递归）
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode nextNode = head.next;
+        ListNode reHead = reverseList1(nextNode);
+        nextNode.next = head;
+        head.next = null;
+        return reHead;
+    }
+
+    /**
+     * 反转链表（迭代）
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    /**
+     * 合并两个有序链表
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        if (l1 == null) {
+            curr.next = l2;
+        }
+        if (l2 == null) {
+            curr.next = l1;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+
+        n1.next = n2;
+        n2.next = n3;
+
+        Solution solution = new Solution();
+        System.out.println(solution.reverseList2(n1).val);
+    }
+
+
 }
