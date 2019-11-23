59bb62473e9bb34302973ec1ab9e958c5a393ed3
yejc <yejc1995@foxmail.com>
2019-11-23 15:45:51 +0800

    feat：排序练习part2

--- src/main/java/sort/Solution.java
+++ src/main/java/sort/Solution.java
@@ -25,4 +25,27 @@ public class Solution {
         }
         System.arraycopy(nums2, 0, nums1, 0, len2 + 1);
     }
+
+    /**
+     * 第一个错误的版本
+     *
+     * @param n
+     * @return
+     */
+    public int firstBadVersion(int n) {
+        int lo = 1, hi = n;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (isBadVersion(mid)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+
+    boolean isBadVersion(int version) {
+        return true;
+    }
 }
