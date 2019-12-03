9dbf92ef41c481cc55a62db8cdad6db4c6744e48
yejc <yejc1995@foxmail.com>
2019-12-03 22:13:23 +0800

    feat：树练习第二遍part2

--- src/main/java/tree/Solution2.java
+++ src/main/java/tree/Solution2.java
@@ -1,5 +1,10 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * @program: pratice
  * @description:
@@ -42,9 +47,6 @@ public class Solution2 {
      * @return
      */
     public boolean isValidBST(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
         return isValidBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
@@ -58,4 +60,82 @@ public class Solution2 {
         return isValidBSTHelper(node.left, lower, node.val) && isValidBSTHelper(node.right, node.val, upper);
     }
 
+    /**
+     * 对称二叉树
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetricHelper(root.left, root.right);
+    }
+
+    private boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
+    }
+
+    /**
+     * 二叉树的层次遍历
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                list.add(node.val);
+            }
+            result.add(list);
+        }
+        return result;
+    }
+
+    /**
+     * 将有序数组转换为二叉搜索树
+     *
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBSTHelper(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBSTHelper(int[] nums, int lo, int hi) {
+        if (lo > hi) {
+            return null;
+        }
+        int mid = lo + (hi - lo) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBSTHelper(nums, lo, mid - 1);
+        node.right = sortedArrayToBSTHelper(nums, mid + 1, hi);
+
+        return node;
+    }
 }
