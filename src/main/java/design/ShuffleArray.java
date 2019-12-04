a5db161c4e91e5ad3a68538b56368688e401ffc7
yejc <yejc1995@foxmail.com>
2019-12-04 21:40:32 +0800

    feat：打乱数组&最小栈

---
+++ src/main/java/design/ShuffleArray.java
@@ -0,0 +1,47 @@
+package design;
+
+import java.util.Random;
+
+/**
+ * 打乱数组
+ *
+ * @program: pratice
+ * @description:
+ * @author: yejc
+ * @create: 2019-12-04 20:42
+ **/
+public class ShuffleArray {
+    private int[] nums;
+
+    public ShuffleArray(int[] nums) {
+        this.nums = nums;
+    }
+
+    /**
+     * Resets the array to its original configuration and return it.
+     */
+    public int[] reset() {
+        return nums;
+    }
+
+    /**
+     * Returns a random shuffling of the array.
+     */
+    public int[] shuffle() {
+        Random random = new Random();
+        int n = nums.length;
+        int[] shuffleArr = new int[n];
+        System.arraycopy(nums, 0, shuffleArr, 0, n);
+        for (int i = 0; i < n; i++) {
+            int r = random.nextInt(n - i) + i;
+            swap(shuffleArr, i, r);
+        }
+        return shuffleArr;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
