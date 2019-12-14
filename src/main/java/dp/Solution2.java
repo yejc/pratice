17a2b9d9c36a411e443098b7ba744a82ae226e70
yejc <yejc1995@foxmail.com>
2019-12-14 18:09:43 +0800

    feat：动态规划第二遍练习part1

---
+++ src/main/java/dp/Solution2.java
@@ -0,0 +1,61 @@
+package dp;
+
+/**
+ * @program: pratice
+ * @description:
+ * @author: yejc
+ * @create: 2019-12-14 10:32
+ **/
+public class Solution2 {
+    /**
+     * 爬楼梯
+     *
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public int climbStairs2(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+
+    /**
+     * 买卖股票的最佳时机
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int maxProfix = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                maxProfix = Math.max(maxProfix, prices[i] - min);
+            }
+        }
+        return maxProfix;
+    }
+}
---
+++ src/main/java/file_search/Condition.java
@@ -0,0 +1,11 @@
+package file_search;
+
+import java.io.File;
+
+/**
+ * @author yejc001
+ * @date 2019-11-04
+ */
+public interface Condition {
+    boolean judge(File file);
+}
