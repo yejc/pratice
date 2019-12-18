ff01fb383693c88108532d3db40510a7976861fb
yejc <yejc1995@foxmail.com>
2019-12-18 21:38:00 +0800

    feat:其他part2

--- src/main/java/other/Solution.java
+++ src/main/java/other/Solution.java
@@ -19,4 +19,42 @@ public class Solution {
         }
         return result;
     }
+
+    /**
+     * 汉明距离
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public int hammingDistance(int x, int y) {
+        int tmp = x ^ y;
+        int mask = 1;
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((tmp & mask) != 0) {
+                result++;
+            }
+            mask <<= 1;
+        }
+        return result;
+    }
+
+    /**
+     * 颠倒二进制位
+     *
+     * @param n
+     * @return
+     */
+    public int reverseBits(int n) {
+        int res = 0;
+        int count = 0;
+        while (count < 32) {
+            res <<= 1;
+            res |= n & 1;
+            n >>= 1;
+            count++;
+        }
+        return res;
+    }
 }
