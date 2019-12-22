7fd6377f3ed4f4213e772178c7bcea8daee54e78
yejc <yejc1995@foxmail.com>
2019-12-22 11:50:12 +0800

    feat:其他part3

--- src/main/java/other/Solution.java
+++ src/main/java/other/Solution.java
@@ -1,5 +1,7 @@
 package other;
 
+import java.util.*;
+
 public class Solution {
 
     /**
@@ -56,5 +58,72 @@ public class Solution {
             count++;
         }
         return res;
+    }
+
+    /**
+     * 帕斯卡三角形
+     *
+     * @param numRows
+     * @return
+     */
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows == 0) {
+            return result;
+        }
+        result.add(Collections.singletonList(1));
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> list = new ArrayList<>();
+            list.add(1);
+            for (int j = 1; j < i; j++) {
+                list.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+            }
+            list.add(1);
+            result.add(list);
+        }
+        return result;
+    }
+
+    /**
+     * 有效的括号
+     *
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!map.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.empty() || map.get(c) != stack.pop()) {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+
+    /**
+     * 缺失数字
+     *
+     * @param nums
+     * @return
+     */
+    public int missingNumber(int[] nums) {
+        int length = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return length;
     }
 }
