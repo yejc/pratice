98849e97260789c949989c3e2825ea5e5b837ea8
yejc <yejc1995@foxmail.com>
2020-01-27 17:46:01 +0800

    feat: 动态规划part2

--- src/main/java/leetcode/middle/dp/Solution.java
+++ src/main/java/leetcode/middle/dp/Solution.java
@@ -25,10 +25,63 @@ public class Solution {
         return dp[nums.length - 1];
     }
 
+    /**
+     * 不同路径
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                if (i == 0) {
+                    dp[i][j] = dp[i][j - 1];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /**
+     * 零钱兑换
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i <= amount; i++) {
+            int cost = Integer.MAX_VALUE;
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0) {
+                    if (dp[i - coins[j]] != Integer.MAX_VALUE) {
+                        cost = Integer.min(cost, dp[i - coins[j]] + 1);
+                    }
+                }
+            }
+            dp[i] = cost;
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+
+
     public static void main(String[] args) {
-        int[] nums = {1, 2};
         Solution solution = new Solution();
-        boolean b = solution.canJump(nums);
+        int[] coins = {2};
+        int b = solution.coinChange(coins, 1);
         System.out.println(b);
     }
+
+
 }
